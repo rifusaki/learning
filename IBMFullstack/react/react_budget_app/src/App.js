766550ff17f82e1cdf3586b89a5e06afc0cac22a
rifusaki <46846373+rifusaki@users.noreply.github.com>
2024-05-09 15:07:44 -0500

    completed budget allocation lab

---
+++ IBMFullstack/react/react_budget_app/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Code to import Budget.js
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem'; 
+import AllocationForm from './components/AllocationForm';
+
+import { AppProvider } from './context/AppContext';
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                    <div className='row mt-3'>
+                        {
+                        // Budget component
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
+                        }        
+                        {
+                        // Remaining component
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
+                        }        
+                        {
+                        // ExpenseTotal component
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                        }  
+                    </div>
+                    <h3 className='mt-3'>Department Budget Allocation</h3>
+                    <div className='row mt-3'>     
+                        {
+                        // ExpenseList component
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>
+                        }
+                    </div>
+                    <h3 className='mt-3'>Change allocation</h3>  
+                    <div className='row mt-3'>         
+                        {/* {
+                        // ExpenseItem component
+                        <div className='col-sm'>
+                            <ExpenseItem />
+                        </div>
+                        }         */}
+                        {
+                        // AllocationForm component
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>
+                        }        
+
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
